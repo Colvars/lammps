713ca0478444bb9e4de31882a6d8a260b12bfd04
Giacomo Fiorin <giacomo.fiorin@gmail.com>
2026-07-22 19:45:13 -0400

    Update the Colvars library to version 2026-07-20
    The following is a list of Colvars pull requests affecting LAMMPS merged since 2025-08-27:
    
    - Improve documentation of spinAngle and tilt
      https://github.com/Colvars/colvars/pull/945 (@giacomofiorin, @jhenin)
    
    - fix: correctly implement the wrap function for orientation_angle
      https://github.com/Colvars/colvars/pull/943 (@HanatoK)
    
    - fix: fix MSVC compilation warning about <stacktrace>
      https://github.com/Colvars/colvars/pull/941 (@HanatoK)
    
    - perf: fix performance regression when using CUDAGM with CPU Colvars
      https://github.com/Colvars/colvars/pull/934 (@HanatoK)
    
    - Update Colvars_traj documentation
      https://github.com/Colvars/colvars/pull/929 (@capritchard)
    
    - refactor: remove unused dq0_1 in calc_fit_forces_impl
      https://github.com/Colvars/colvars/pull/927 (@HanatoK)
    
    - fix: the macros of determining the device code should be reversed
      https://github.com/Colvars/colvars/pull/917 (@HanatoK)
    
    - Backport updates to LAMMPS interface files
      https://github.com/Colvars/colvars/pull/916 (@giacomofiorin)
    
    - colvars_grid improvements
      https://github.com/Colvars/colvars/pull/912 (@jhenin)
    
    - Simplify and optimize coordination numbers
      https://github.com/Colvars/colvars/pull/909 (@giacomofiorin, @HanatoK)
    
    - Do not require harmonic walls start with nonzero k
      https://github.com/Colvars/colvars/pull/906 (@jhenin)
    
    - Fix: Implement numerically safe switching function
      https://github.com/Colvars/colvars/pull/904 (@jhenin, @giacomofiorin)
    
    - fix: initialize variable, appease Coverity
      https://github.com/Colvars/colvars/pull/901 (@jhenin)
    
    - fix: fix the out-of-bound access in from_simple_string
      https://github.com/Colvars/colvars/pull/897 (@HanatoK)
    
    - doc: urge users the importance of specifying names for sub-CVs
      https://github.com/Colvars/colvars/pull/896 (@HanatoK)
    
    - Warn when discarding state of unknown object
      https://github.com/Colvars/colvars/pull/893 (@jhenin, @giacomofiorin)
    
    - Refactor: replace static colvarmodule with class member
      https://github.com/Colvars/colvars/pull/886 (@jhenin, @HanatoK)
    
    - Trap floating-point exceptions
      https://github.com/Colvars/colvars/pull/877 (@jhenin, @HanatoK)
    
    - Fix debug gradients recalc gradients
      https://github.com/Colvars/colvars/pull/868 (@HanatoK)
    
    - Make non-MTS colvars always awake
      https://github.com/Colvars/colvars/pull/866 (@jhenin)
    
    - Fix cut and paste error in dipole_angle gradients
      https://github.com/Colvars/colvars/pull/856 (@giacomofiorin)
    
    - fix: update the total charge and total mass from AOS to SOA
      https://github.com/Colvars/colvars/pull/854 (@HanatoK)
    
    - Remove legacy Makefile support for LAMMPS package
      https://github.com/Colvars/colvars/pull/853 (@giacomofiorin)
    
    - Moving harmonic walls - unify moving restraint logic
      https://github.com/Colvars/colvars/pull/851 (@jhenin, @giacomofiorin, @OmriShf)
    
    - opt: optimize the forces or gradients from q to the fitting group
      https://github.com/Colvars/colvars/pull/849 (@HanatoK)
    
    - Define version numbers outside of headers
      https://github.com/Colvars/colvars/pull/847 (@giacomofiorin)
    
    - Optimize the calculation of fit gradients and update the documentation (fix #560)
      https://github.com/Colvars/colvars/pull/839 (@HanatoK, @jhenin)
    
    - Loop over replicas when computing well-tempered metadynamics scaling factor
      https://github.com/Colvars/colvars/pull/820 (@giacomofiorin)
    
    Authors: @capritchard, @giacomofiorin, @HanatoK, @jhenin, @OmriShf

--- lib/colvars/colvarproxy_system.cpp
+++ lib/colvars/colvarproxy_system.cpp
@@ -7,6 +7,7 @@
 // If you wish to distribute your changes, please submit them to the
 // Colvars repository at GitHub.
 
+#include <algorithm>
 
 #include "colvarmodule.h"
 #include "colvartypes.h"
@@ -72,7 +73,7 @@ void colvarproxy_system::add_energy(cvm::real /* energy */) {}
 void colvarproxy_system::request_total_force(bool yesno)
 {
   if (yesno == true)
-    cvm::error("Error: total forces are currently not implemented.\n",
+    cvm::error_static("Error: total forces are currently not implemented.\n",
                COLVARS_NOT_IMPLEMENTED);
 }
 
@@ -89,19 +90,13 @@ bool colvarproxy_system::total_forces_same_step() const
 }
 
 
-inline int round_to_integer(cvm::real x)
-{
-  return int(cvm::floor(x+0.5));
-}
-
-
 void colvarproxy_system::update_pbc_lattice()
 {
   // Periodicity is assumed in all directions
 
   if (boundaries_type == boundaries_unsupported ||
       boundaries_type == boundaries_non_periodic) {
-    cvm::error("Error: setting PBC lattice with unsupported boundaries.\n",
+    cvm::error_static("Error: setting PBC lattice with unsupported boundaries.\n",
                COLVARS_BUG_ERROR);
     return;
   }
@@ -133,35 +128,15 @@ void colvarproxy_system::reset_pbc_lattice()
 
 
 cvm::rvector colvarproxy_system::position_distance(cvm::atom_pos const &pos1,
-                                                   cvm::atom_pos const &pos2)
-  const
+                                                   cvm::atom_pos const &pos2) const
 {
-  if (boundaries_type == boundaries_unsupported) {
-    cvm::error("Error: unsupported boundary conditions.\n", COLVARS_INPUT_ERROR);
-  }
-
-  cvm::rvector diff = (pos2 - pos1);
-
-  if (boundaries_type == boundaries_non_periodic) return diff;
-
-  cvm::real const x_shift = round_to_integer(reciprocal_cell_x*diff);
-  cvm::real const y_shift = round_to_integer(reciprocal_cell_y*diff);
-  cvm::real const z_shift = round_to_integer(reciprocal_cell_z*diff);
-
-  diff.x -= x_shift*unit_cell_x.x + y_shift*unit_cell_y.x +
-    z_shift*unit_cell_z.x;
-  diff.y -= x_shift*unit_cell_x.y + y_shift*unit_cell_y.y +
-    z_shift*unit_cell_z.y;
-  diff.z -= x_shift*unit_cell_x.z + y_shift*unit_cell_y.z +
-    z_shift*unit_cell_z.z;
-
-  return diff;
+  return position_distance_internal(pos1, pos2);
 }
 
 
 int colvarproxy_system::get_molid(int &)
 {
-  cvm::error("Error: only VMD allows the use of multiple \"molecules\", "
+  cvm::error_static("Error: only VMD allows the use of multiple \"molecules\", "
              "i.e. multiple molecular systems.", COLVARS_NOT_IMPLEMENTED);
   return -1;
 }
@@ -169,7 +144,7 @@ int colvarproxy_system::get_molid(int &)
 
 int colvarproxy_system::get_alch_lambda(cvm::real * /* lambda */)
 {
-  return cvm::error("Error in get_alch_lambda: alchemical lambda dynamics is not supported by this build.",
+  return cvm::error_static("Error in get_alch_lambda: alchemical lambda dynamics is not supported by this build.",
     COLVARS_NOT_IMPLEMENTED);
 }
 
@@ -183,27 +158,27 @@ void colvarproxy_system::set_alch_lambda(cvm::real lambda)
 
 int colvarproxy_system::send_alch_lambda()
 {
-  return cvm::error("Error in set_alch_lambda: alchemical lambda dynamics is not supported by this build.",
+  return cvm::error_static("Error in set_alch_lambda: alchemical lambda dynamics is not supported by this build.",
     COLVARS_NOT_IMPLEMENTED);
 }
 
 
 int colvarproxy_system::get_dE_dlambda(cvm::real * /* force */)
 {
-  return cvm::error("Error in get_dE_dlambda: alchemical lambda dynamics is not supported by this build.",
+  return cvm::error_static("Error in get_dE_dlambda: alchemical lambda dynamics is not supported by this build.",
     COLVARS_NOT_IMPLEMENTED);
 }
 
 
 int colvarproxy_system::apply_force_dE_dlambda(cvm::real* /* force */)
 {
-  return cvm::error("Error in apply_force_dE_dlambda: function is not implemented by this build.",
+  return cvm::error_static("Error in apply_force_dE_dlambda: function is not implemented by this build.",
     COLVARS_NOT_IMPLEMENTED);
 }
 
 
 int colvarproxy_system::get_d2E_dlambda2(cvm::real*)
 {
-  return cvm::error("Error in get_d2E_dlambda2: function is not implemented by this build.",
+  return cvm::error_static("Error in get_d2E_dlambda2: function is not implemented by this build.",
     COLVARS_NOT_IMPLEMENTED);
 }
